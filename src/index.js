78d00a11f4c4312f7e728a2cf54336c226dc7ec9
zuffidries <zuffidries@gmail.com>
2017-04-10 23:24:48 -0400

    Simple

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import $ from 'jquery';
-
-
-$(document).ready(function(){
-
-                var pic1 = $(".image1").attr('src');
-
-
-                $(".image1").hover(function() {
-                    $(this).attr("src","photos/jumperwet.jpg");
-                        }, function() {
-                    $(this).attr("src", pic1);
-                });
-            });
